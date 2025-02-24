14ac14f051de663e5552f6f80054a6597a872a5f
ChristyBro <kpf0x96oyha8@hotmail.com>
2025-02-24 17:14:21 +0800

    more test

--- test/TimeStringStorage.test.js
+++ test/TimeStringStorage.test.js
@@ -15,6 +15,11 @@ describe("TimeStringStorage", function () {
     });
 
     describe("Store Records", function () {
+        beforeEach(async function () {
+            const stakeAmount = ethers.utils.parseEther("0.1");
+            await timeStringStorage.connect(owner).stake({ value: stakeAmount });
+        });
+
         it("Should store a record with current timestamp", async function () {
             const testData = "Hello World";
             
@@ -62,6 +67,9 @@ describe("TimeStringStorage", function () {
         });
 
         it("Should return all user records", async function () {
+            const stakeAmount = ethers.utils.parseEther("0.1");
+            await timeStringStorage.connect(owner).stake({ value: stakeAmount });
+            
             const testData1 = "Record 1";
             const testData2 = "Record 2";
             
@@ -77,6 +85,9 @@ describe("TimeStringStorage", function () {
         it("Should return correct record count", async function () {
             expect(await timeStringStorage.getRecordCount()).to.equal(0);
             
+            const stakeAmount = ethers.utils.parseEther("0.1");
+            await timeStringStorage.connect(owner).stake({ value: stakeAmount });
+            
             await timeStringStorage.storeRecord("Test");
             expect(await timeStringStorage.getRecordCount()).to.equal(1);
             
@@ -88,14 +99,163 @@ describe("TimeStringStorage", function () {
             await expect(timeStringStorage.getRecord(0))
                 .to.be.revertedWith("Index out of bounds");
             
+            const stakeAmount = ethers.utils.parseEther("0.1");
+            await timeStringStorage.connect(owner).stake({ value: stakeAmount });
+            
             await timeStringStorage.storeRecord("Test");
             await expect(timeStringStorage.getRecord(1))
                 .to.be.revertedWith("Index out of bounds");
         });
     });
 
+    describe("Staking", function () {
+        it("Should allow staking with correct amount", async function () {
+            const stakeAmount = ethers.utils.parseEther("0.1");
+            
+            await expect(timeStringStorage.connect(addr1).stake({ value: stakeAmount }))
+                .to.emit(timeStringStorage, "Staked")
+                .withArgs(addr1.address, stakeAmount, anyValue);
+            
+            const [amount, stakeTimestamp, lastDiaryTimestamp, isActive] = await timeStringStorage.getStakeInfo(addr1.address);
+            expect(amount).to.equal(stakeAmount);
+            expect(stakeTimestamp).to.be.gt(0);
+            expect(lastDiaryTimestamp).to.equal(0);
+            expect(isActive).to.be.true;
+        });
+
+        it("Should reject incorrect stake amount", async function () {
+            const wrongAmount = ethers.utils.parseEther("0.05");
+            
+            await expect(timeStringStorage.connect(addr1).stake({ value: wrongAmount }))
+                .to.be.revertedWith("Must stake exactly 0.1 ETH");
+        });
+
+        it("Should reject double staking", async function () {
+            const stakeAmount = ethers.utils.parseEther("0.1");
+            
+            await timeStringStorage.connect(addr1).stake({ value: stakeAmount });
+            
+            await expect(timeStringStorage.connect(addr1).stake({ value: stakeAmount }))
+                .to.be.revertedWith("Already staked");
+        });
+
+        it("Should require staking before storing records", async function () {
+            await expect(timeStringStorage.connect(addr1).storeRecord("Test"))
+                .to.be.revertedWith("Must stake 0.1 ETH first");
+        });
+
+        it("Should allow storing records after staking", async function () {
+            const stakeAmount = ethers.utils.parseEther("0.1");
+            await timeStringStorage.connect(addr1).stake({ value: stakeAmount });
+            
+            await expect(timeStringStorage.connect(addr1).storeRecord("Test"))
+                .to.emit(timeStringStorage, "RecordStored");
+            
+            const [amount, stakeTimestamp, lastDiaryTimestamp, isActive] = await timeStringStorage.getStakeInfo(addr1.address);
+            expect(lastDiaryTimestamp).to.be.gt(0);
+        });
+    });
+
+    describe("Withdrawal", function () {
+        beforeEach(async function () {
+            const stakeAmount = ethers.utils.parseEther("0.1");
+            await timeStringStorage.connect(addr1).stake({ value: stakeAmount });
+        });
+
+        it("Should allow withdrawal after diary interval without diary entry", async function () {
+            await ethers.provider.send("evm_increaseTime", [7 * 24 * 60 * 60 + 1]);
+            await ethers.provider.send("evm_mine");
+            
+            expect(await timeStringStorage.canWithdraw(addr1.address)).to.be.true;
+            
+            const balanceBefore = await addr1.getBalance();
+            const tx = await timeStringStorage.connect(addr1).withdraw();
+            const receipt = await tx.wait();
+            const gasUsed = receipt.gasUsed.mul(receipt.effectiveGasPrice);
+            const balanceAfter = await addr1.getBalance();
+            
+            expect(balanceAfter.add(gasUsed).sub(balanceBefore)).to.equal(ethers.utils.parseEther("0.1"));
+            
+            const [amount, stakeTimestamp, lastDiaryTimestamp, isActive] = await timeStringStorage.getStakeInfo(addr1.address);
+            expect(amount).to.equal(0);
+            expect(isActive).to.be.false;
+        });
+
+        it("Should allow withdrawal within diary interval after diary entry", async function () {
+            await timeStringStorage.connect(addr1).storeRecord("Daily diary");
+            
+            expect(await timeStringStorage.canWithdraw(addr1.address)).to.be.true;
+            
+            await expect(timeStringStorage.connect(addr1).withdraw())
+                .to.emit(timeStringStorage, "Withdrawn")
+                .withArgs(addr1.address, ethers.utils.parseEther("0.1"), anyValue);
+        });
+
+        it("Should reject withdrawal before diary interval without diary entry", async function () {
+            expect(await timeStringStorage.canWithdraw(addr1.address)).to.be.false;
+            
+            await expect(timeStringStorage.connect(addr1).withdraw())
+                .to.be.revertedWith("Cannot withdraw yet - diary requirement not met");
+        });
+
+        it("Should reject withdrawal after diary interval expires", async function () {
+            await timeStringStorage.connect(addr1).storeRecord("Daily diary");
+            
+            await ethers.provider.send("evm_increaseTime", [7 * 24 * 60 * 60 + 1]);
+            await ethers.provider.send("evm_mine");
+            
+            expect(await timeStringStorage.canWithdraw(addr1.address)).to.be.false;
+            
+            await expect(timeStringStorage.connect(addr1).withdraw())
+                .to.be.revertedWith("Cannot withdraw yet - diary requirement not met");
+        });
+
+        it("Should reject withdrawal when not staked", async function () {
+            await expect(timeStringStorage.connect(addr2).withdraw())
+                .to.be.revertedWith("No active stake");
+        });
+    });
+
+    describe("Time Until Withdrawal", function () {
+        beforeEach(async function () {
+            const stakeAmount = ethers.utils.parseEther("0.1");
+            await timeStringStorage.connect(addr1).stake({ value: stakeAmount });
+        });
+
+        it("Should return 0 when withdrawal is allowed", async function () {
+            await ethers.provider.send("evm_increaseTime", [7 * 24 * 60 * 60 + 1]);
+            await ethers.provider.send("evm_mine");
+            
+            expect(await timeStringStorage.timeUntilWithdrawal(addr1.address)).to.equal(0);
+        });
+
+        it("Should return time remaining until diary interval", async function () {
+            const timeUntil = await timeStringStorage.timeUntilWithdrawal(addr1.address);
+            expect(timeUntil).to.be.gt(0);
+            expect(timeUntil).to.be.lte(7 * 24 * 60 * 60);
+        });
+
+        it("Should return max uint256 when diary interval expired", async function () {
+            await timeStringStorage.connect(addr1).storeRecord("Daily diary");
+            
+            await ethers.provider.send("evm_increaseTime", [7 * 24 * 60 * 60 + 1]);
+            await ethers.provider.send("evm_mine");
+            
+            const maxUint256 = ethers.constants.MaxUint256;
+            expect(await timeStringStorage.timeUntilWithdrawal(addr1.address)).to.equal(maxUint256);
+        });
+
+        it("Should return 0 for non-staked user", async function () {
+            expect(await timeStringStorage.timeUntilWithdrawal(addr2.address)).to.equal(0);
+        });
+    });
+
     describe("User Separation", function () {
         it("Should separate records by user address", async function () {
+            const stakeAmount = ethers.utils.parseEther("0.1");
+            await timeStringStorage.connect(owner).stake({ value: stakeAmount });
+            await timeStringStorage.connect(addr1).stake({ value: stakeAmount });
+            
             const ownerData = "Owner Data";
             const addr1Data = "Addr1 Data";
             
@@ -112,6 +272,10 @@ describe("TimeStringStorage", function () {
         });
 
         it("Should return correct count for each user", async function () {
+            const stakeAmount = ethers.utils.parseEther("0.1");
+            await timeStringStorage.connect(owner).stake({ value: stakeAmount });
+            await timeStringStorage.connect(addr1).stake({ value: stakeAmount });
+            
             await timeStringStorage.connect(owner).storeRecord("Owner 1");
             await timeStringStorage.connect(owner).storeRecord("Owner 2");
             await timeStringStorage.connect(addr1).storeRecord("Addr1 1");
